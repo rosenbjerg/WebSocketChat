34df5b0844c0e2d3ab7c2f04d213536171165f06
rosenbjerg <malterandersen@live.dk>
2017-02-26 20:29:35 +0100

    git

---
+++ ChatRoom/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using RHttpServer;
+
+namespace ChatRoom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new TaskBasedHttpServer(5002);
+            var rman = new RoomManager();
+
+            server.Get("/", (req, res) =>
+            {
+                res.RenderPage("pages/index.ecs", new RenderParams
+                {
+                    {"url", "Main"}
+                });
+            });
+
+            server.Get("/:room", (req, res) =>
+            {
+                var room = HttpUtility.UrlDecode(req.Params["room"]);
+                res.RenderPage("pages/index.ecs", new RenderParams
+                {
+                    {"url", room}
+                });
+            });
+
+            server.WebSocket("/:room", (req, wsd) =>
+            {
+                var room = HttpUtility.UrlDecode(req.Params["room"]).ToLowerInvariant();
+                rman.Join(room, wsd);
+            });
+
+            server.Start(true);
+        }
+    }
+}
